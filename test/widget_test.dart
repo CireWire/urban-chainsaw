5a73b82638fba59d5fe7763a3dc89475b9ea98d3
Eric Gutierrez Jr <17133321+CireWire@users.noreply.github.com>
2023-05-23 18:51:14 -0700

    Add files via upload

---
+++ test/widget_test.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Counter App'),
+        ),
+        body: const Center(
+          child: CounterWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  const CounterWidget({Key? key}) : super(key: key);
+
+  @override
+  _CounterWidgetState createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          '$_counter',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+        ElevatedButton(
+          onPressed: _incrementCounter,
+          child: const Icon(Icons.add),
+        ),
+      ],
+    );
+  }
+}
